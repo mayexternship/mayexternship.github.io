039fffda4e88fea3ce4266f36d6bcdd11eaffbb6
mmmaaayyy <mmaayy09@gmail.com>
2016-06-19 17:49:24 -0400

    1

--- game.js
+++ game.js
@@ -2,17 +2,14 @@
 function functionStart(id)
 {
  	document.getElementById("smallscreen").style.backgroundImage = "url('scenetest.png')";
+document.getElementById("glassesButton").style.display = "block";
+
 var e = document.getElementById(id); 
     if(e.style.display == "none")
     e.style.display = "block";
 	else
     e.style.display = "none";
     var e = document.getElementById("instructions"); 
-    if(e.style.display == "none")
-    e.style.display = "block";
-	else
-    e.style.display = "none";
-var e = document.getElementById("glassesButton"); 
     if(e.style.display == "none")
     e.style.display = "block";
 	else
@@ -40,4 +37,9 @@ var e = document.getElementById("scenario1");
 	else
     e.style.display = "none";
 $('#answer1').show(); return false;
+var e = document.getElementById("glassesButton"); 
+    if(e.style.display == "none")
+    e.style.display = "block";
+	else
+    e.style.display = "none";
 }
