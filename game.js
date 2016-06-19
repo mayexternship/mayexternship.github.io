3dcbdb6eccdaf5d07cd352025206255ab79540a8
mmmaaayyy <mmaayy09@gmail.com>
2016-06-19 17:12:25 -0400

    h

--- game.js
+++ game.js
@@ -21,19 +21,3 @@ var e = document.getElementById("entername");
 $('#scenario1').show(); return false;
 }
 
-function functionStart(id)
-{
- 	document.getElementById("smallscreen").style.backgroundImage = "url('test.png')";
-document.getElementById("nextButton").style.display = "block"; //show next
-var e = document.getElementById(id); 
-    if(e.style.display == "none")
-    e.style.display = "block";
-	else
-    e.style.display = "none";
-var e = document.getElementById("scenario1"); 
-    if(e.style.display == "none")
-    e.style.display = "block";
-	else
-    e.style.display = "none";
-$('#answer1').show(); return false;
-}
