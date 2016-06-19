ef6bfab846cede0b8352907071c6ae96b6e2ce73
mmmaaayyy <mmaayy09@gmail.com>
2016-06-19 17:15:38 -0400

    d

--- game.js
+++ game.js
@@ -21,3 +21,19 @@ var e = document.getElementById("entername");
 $('#scenario1').show(); return false;
 }
 
+function function2(id)
+{
+ 	document.getElementById("smallscreen").style.backgroundImage = "url('test.png')";
+document.getElementById("nextButton").style.display = "block"; //show next
+var e = document.getElementById(id); 
+    if(e.style.display == "none")
+    e.style.display = "block";
+	else
+    e.style.display = "none";
+var e = document.getElementById("scenario1"); 
+    if(e.style.display == "none")
+    e.style.display = "block";
+	else
+    e.style.display = "none";
+$('#answer1').show(); return false;
+}
